b005d73a6ce589682025213d9acc502f33632a13
Or <ororgod@gmail.com>
2020-02-02 17:30:42 +0200

    this is my first commit

---
+++ src/test/java/Qaclickacademy/SeleniumTest.java
@@ -0,0 +1,27 @@
+package Qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	
+	
+	
+	@Test
+	public void BrowserAutomation() 
+	{
+		
+		System.out.println("BrowserAutomation");
+
+		
+	}
+	
+	
+	@Test
+	public void elementUI() 
+	{
+		
+		System.out.println("elementUI");
+		
+	}
+}
